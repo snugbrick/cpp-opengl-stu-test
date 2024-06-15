11e1a5b53b1b5a65e98b9981ad6bfe4264094a02
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-06-16 07:01:44 +0800

    3hours

--- cpp2/cppProject/src/glsl/vs.glsl
+++ cpp2/cppProject/src/glsl/vs.glsl
@@ -6,8 +6,12 @@ layout(location = 2) in vec2 aTexCoord;
 out vec3 ourColor;
 out vec2 TexCoord;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     ourColor = aColor;
-    TexCoord = aTexCoord;
+    TexCoord = vec2(aTexCoord.x, 1.0 - aTexCoord.y);
 }
--- cpp2/cppProject/src/main.cpp
+++ cpp2/cppProject/src/main.cpp
@@ -55,11 +55,47 @@ int main()
 
     float vertices[] = {
         //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-        0.5f, 0.5f, 0.0f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,   // 右上
-        0.5f, -0.5f, 0.0f, /**/ 0.0f, 1.0f, 0.0f, /**/ 1.0f, 0.0f,  // 右下
-        -0.5f, -0.5f, 0.0f, /**/ 0.0f, 0.0f, 1.0f, /**/ 0.0f, 0.0f, // 左下
-        -0.5f, 0.5f, 0.0f, /**/ 1.0f, 1.0f, 0.0f, /**/ 0.0f, 1.0f   // 左上
-    };
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        -0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        /**/
+        -0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        /**/
+        -0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        /**/
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        /**/
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        /**/
+        -0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f,
+        0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f, /**/ 1.0f, 0.0f, 0.0f, /**/ 0.0f, 1.0f};
 
     unsigned int indices[] = {
         0, 1, 3, // first triangle
@@ -140,20 +176,27 @@ int main()
     glUniform1i(glGetUniformLocation(shading.ID, "texture"), 0);
     shading.setInt("texture2", 1);
 
-    // mat and vec
-    glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f);
-    glm::mat4 trans = glm::mat4(1.0f);
-
-    vec = trans * vec;
-    std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+    // vecs
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
     //  main=============================================================================
     // life circle
+    glEnable(GL_DEPTH_TEST);
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
@@ -164,8 +207,35 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
+        // create transformations :/
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        unsigned int modelLoc = glGetUniformLocation(shading.ID, "model");
+        unsigned int viewLoc = glGetUniformLocation(shading.ID, "view");
+
+        // glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        shading.setMat4("projection", projection);
+
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shading.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        // glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f); // 透视平截头体
+        // mat and vec============================
+        // glDrawArrays(GL_TRIANGLES, 0, 36);
 
         // events and change buffer
         glfwSwapBuffers(window);
